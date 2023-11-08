249ef06faf05d76e5d5b35f09e98d268a64c69af
epiccod <147839812+epiccod@users.noreply.github.com>
2023-11-07 22:05:47 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    createGrid(16)
+})
+
+const etchDiv = document.getElementById('etch')
+etchDiv.addEventListener('mouseover', etchADiv)
+
+function createGrid(gridSize) {
+    for (i = 0; i < Math.pow(gridSize,2); i++) {
+        let gridDiv = document.createElement('div')
+        document.getElementById('etch').appendChild(gridDiv)
+    }
+}
+
+function etchADiv(e) {
+    if (e.target == etchDiv) { return }
+    e.target.classList.add('etched')
+}
+
+function reset() {
+    while (etchDiv.firstChild) {
+        etchDiv.removeChild(etchDiv.firstChild)
+    }
+    let numSquares = prompt('How many pixels on each side')
+    etchDiv.style.gridTemplateColumns = 'repeat(' + numSquares + ', 1fr)'
+    etchDiv.style.gridTemplateRows = 'repeat(' + numSquares + ', 1fr)'
+    createGrid(numSquares)
+}
